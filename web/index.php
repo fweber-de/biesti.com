6dac0c446a0644ed7b1cd00f9ee91e62acf2a371
Florian Weber <florian.weber.dd@me.com>
2014-05-28 10:16:07 +0200

    imprint

--- web/index.php
+++ web/index.php
@@ -16,14 +16,14 @@ $app->get('/portfolio', function () use ($app) {
     return $app['twig']->render('portfolio.html.twig');
 });
 
-$app->get('/kontakt', function () use ($app) {
-    return $app['twig']->render('kontakt.html.twig');
+$app->get('/contact', function () use ($app) {
+    return $app['twig']->render('contact.html.twig');
 });
 
-$app->get('/impressum', function () use ($app) {
-    return $app['twig']->render('impressum.html.twig');
+$app->get('/imprint', function () use ($app) {
+    return $app['twig']->render('imprint.html.twig');
 });
 
-$app['debug'] = true;
+$app['debug'] = false;
 
 $app->run();
