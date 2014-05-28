6c18cc2982d62f1f163e223d7ae21acee7a071fd
fweber-de <florian.weber.dd@icloud.com>
2014-05-28 14:03:41 +0200

    index

--- web/assets/js/app.js
+++ web/assets/js/app.js
@@ -10,5 +10,14 @@ $(document).ready(function() {
     //active links
     $('#nl-' + currentPage).addClass('active');
     $('#nbl-' + currentPage).addClass('active');
+    
+    var picn = 2;
+    
+    setInterval(function() {
+        $('.img-profile').attr('src', $('#src-pi-' + picn).html());
+        picn++;
+        
+        if(picn > $('#profile-img-src-box').children().length) picn = 1;
+    }, 5000);
 
 });
