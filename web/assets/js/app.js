e6a7e5f10dc9c7cca83fd4360de5601c48074462
Florian Weber <florian.weber.dd@icloud.com>
2015-03-21 12:20:27 +0100

    dev

---
+++ web/assets/js/app.js
@@ -0,0 +1,31 @@
+/*
+ * biesti.com JS
+ * © 2014, FLorian Weber
+ */
+
+$(document).ready(function() {
+
+    var currentPage = $('#current-page').html();
+
+    //active links
+    $('#nl-' + currentPage).addClass('active');
+    $('#nbl-' + currentPage).addClass('active');
+    
+    var picn = 2;
+    
+    setInterval(function() {
+        $('.img-profile').attr('src', $('#src-pi-' + picn).html());
+        picn++;
+        
+        if(picn > $('#profile-img-src-box').children().length) picn = 1;
+    }, 5000);
+
+    //public key
+    var publicKey = null;
+
+    $.get('https://keybase.io/fweber/key.asc', function(data) {
+        publicKey = data;
+        $('#key-area').html(data);
+    });
+
+});
