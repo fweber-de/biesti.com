d4cdafbcce26446deba31c46d79f19b91c484df1
Florian Weber <florian.weber.dd@icloud.com>
2015-03-31 16:55:01 +0200

    persist post

--- src/fweber/BackendBundle/Controller/PostController.php
+++ src/fweber/BackendBundle/Controller/PostController.php
@@ -2,8 +2,11 @@
 
 namespace fweber\BackendBundle\Controller;
 
+use fweber\BackendBundle\Component\ApiMessage;
+use fweber\DataBundle\Entity\Post;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 /**
  * @author Florian Weber <florian.weber@fweber.info>
@@ -14,14 +17,66 @@ class PostController extends Controller
     {
         $posts = $this->getDoctrine()->getRepository('fweberDataBundle:Post')->findAll();
 
-        return $this->render('fweberBackendBundle:Post:collection.html.twig', array(
-            'posts' => $posts,
-        ));
+        return $this->render(
+            'fweberBackendBundle:Post:collection.html.twig',
+            array(
+                'posts' => $posts,
+            )
+        );
     }
 
     public function createAction(Request $request)
     {
+        //TODO: implement draft, publish date
+
         if ($request->get('sent', 0) == 1) {
+            $post = new Post();
+            $post->setTitle($request->get('title'))
+                ->setText($request->get('text'))
+                ->setIsDraft(false)
+                ->setOpenDate(new \DateTime('now'))
+                ->setPublishDate(new \DateTime('now'));
+
+            //validation
+            $validator = $this->get('validator');
+            $errors = $validator->validate($post);
+
+            //error handling
+            if (count($errors) > 0) {
+                if ($request->get('ajax', 0) == 1) {
+                    $message = new ApiMessage();
+                    $message->message = (string)$errors;
+                    $message->status = ApiMessage::STATUS_ERROR;
+
+                    $response = new Response(json_encode($message));
+                    $response->setStatusCode(400);
+
+                    return $response;
+                } else {
+                    foreach ($errors as $error) {
+                        $this->addFlash('error', $error);
+                    }
+
+                    return $this->render('fweberBackendBundle:Post:create.html.twig');
+                }
+            }
+
+            $em = $this->getDoctrine()->getEntityManager();
+            $em->persist($post);
+            $em->flush();
+
+            if ($request->get('ajax', 0) == 1) {
+                $message = new ApiMessage();
+                $message->message = 'Post saved!';
+                $message->status = ApiMessage::STATUS_SUCCESS;
+
+                $response = new Response(json_encode($message));
+                $response->setStatusCode(200);
+
+                return $response;
+            } else {
+                $this->redirectToRoute('backend_posts_collection');
+            }
         }
 
         return $this->render('fweberBackendBundle:Post:create.html.twig');
