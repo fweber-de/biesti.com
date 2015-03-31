b52b573873696491a86765c5a4073f067d56b846
Florian Weber <florian.weber.dd@icloud.com>
2015-03-31 16:29:42 +0200

    fix

--- deploy-linux.sh
+++ deploy-linux.sh
@@ -3,7 +3,7 @@ git pull
 php app/console doctrine:schema:update --force
 php app/console cache:clear --env=prod --no-debug
 bower install --allow-root
-php app/console assetic:dump
+php app/console assetic:dump --env=prod --no-debug
 composer.phar install
 cd ..
 chmod 777 -R fweber.info
