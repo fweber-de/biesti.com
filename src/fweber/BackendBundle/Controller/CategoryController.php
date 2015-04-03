6f74fabc94d36029417a8f1d306b9c8e0470a1b3
Florian Weber <florian.weber.dd@icloud.com>
2015-04-03 16:53:53 +0200

    update, delete tag

--- src/fweber/BackendBundle/Controller/CategoryController.php
+++ src/fweber/BackendBundle/Controller/CategoryController.php
@@ -36,13 +36,13 @@ class CategoryController extends Controller
     public function createAction(Request $request)
     {
         if ($request->get('sent', 0) == 1) {
-            $category = new Category();
-            $category->setTitle($request->get('title'))
+            $tag = new Category();
+            $tag->setTitle($request->get('title'))
                 ->setSlug(SlugGenerator::generate($request->get('title')));
 
             //validation
             $validator = $this->get('validator');
-            $errors = $validator->validate($category);
+            $errors = $validator->validate($tag);
 
             //error handling
             if (count($errors) > 0) {
@@ -65,7 +65,7 @@ class CategoryController extends Controller
             }
 
             $em = $this->getDoctrine()->getEntityManager();
-            $em->persist($category);
+            $em->persist($tag);
             $em->flush();
 
             if ($request->get('ajax', 0) == 1) {
@@ -83,5 +83,95 @@ class CategoryController extends Controller
         }
 
         return $this->render('@fweberBackend/Category/create.html.twig');
+    }
+
+    public function updateAction(Request $request, $tagId)
+    {
+        $tag = $this->getDoctrine()->getRepository('fweberDataBundle:Category')->findOneById($tagId);
+
+        if (!$tag) {
+            throw new \InvalidArgumentException('Tag not found!');
+        }
+
+        if ($request->get('sent', 0) == 1) {
+            $tag->setTitle($request->get('title'))
+                ->setSlug(SlugGenerator::generate($request->get('title')));
+
+            //validation
+            $validator = $this->get('validator');
+            $errors = $validator->validate($tag);
+
+            //error handling
+            if (count($errors) > 0) {
+                if ($request->get('ajax', 0) == 1) {
+                    $message = new ApiMessage();
+                    $message->message = (string)$errors;
+                    $message->status = ApiMessage::STATUS_ERROR;
+
+                    $response = new Response(json_encode($message));
+                    $response->setStatusCode(400);
+
+                    return $response;
+                } else {
+                    foreach ($errors as $error) {
+                        $this->addFlash('error', $error);
+                    }
+
+                    return $this->render(
+                        'fweberBackendBundle:Category:update.html.twig',
+                        array(
+                            'tag' => $tag
+                        )
+                    );
+                }
+            }
+
+            $em = $this->getDoctrine()->getEntityManager();
+            $em->flush();
+
+            if ($request->get('ajax', 0) == 1) {
+                $message = new ApiMessage();
+                $message->message = 'Tag saved!';
+                $message->status = ApiMessage::STATUS_SUCCESS;
+
+                $response = new Response(json_encode($message));
+                $response->setStatusCode(200);
+
+                return $response;
+            } else {
+                return $this->redirectToRoute('backend_tags_collection');
+            }
+        }
+
+        return $this->render(
+            '@fweberBackend/Category/update.html.twig',
+            array(
+                'tag' => $tag
+            )
+        );
+    }
+
+    public function deleteAction(Request $request, $tagId)
+    {
+        $tag = $this->getDoctrine()->getRepository('fweberDataBundle:Category')->findOneById($tagId);
+
+        if (!$tag) {
+            throw new \InvalidArgumentException('Tag not found!');
+        }
+
+        if ($request->get('sent', 0) == 1) {
+            $em = $this->getDoctrine()->getEntityManager();
+            $em->remove($tag);
+            $em->flush();
+
+            return $this->redirectToRoute('backend_tags_collection');
+        }
+
+        return $this->render(
+            'fweberBackendBundle:Category:delete.html.twig',
+            array(
+                'tag' => $tag,
+            )
+        );
     }
 }
