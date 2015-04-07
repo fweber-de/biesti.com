050f44ed4b2460a046154d41f765ea459a1f81bb
Florian Weber <florian.weber.dd@icloud.com>
2015-04-07 20:32:22 +0200

    img fix

--- web/assets/js/post.js
+++ web/assets/js/post.js
@@ -2,4 +2,6 @@ $(document).ready(function () {
     $('.post h1, .post h2, .post h3, .post h4').anchorify({
         text: '<i class="fa fa-link"></i>'
     });
+
+    $('.post img').addClass('img-responsive img-thumbnail');
 });
