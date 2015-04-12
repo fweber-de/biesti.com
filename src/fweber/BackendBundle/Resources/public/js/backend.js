cc1186a6a7f0587c4d732bdba73f99044b9d33b1
Florian Weber <florian.weber.dd@icloud.com>
2015-04-12 15:15:45 +0200

    move backend assets

---
+++ src/fweber/BackendBundle/Resources/public/js/backend.js
@@ -0,0 +1,13 @@
+/*
+ * fweber.info JS
+ * © 2015, FLorian Weber
+ */
+
+$(document).ready(function () {
+
+    var currentPage = $('#current-page').html();
+
+    //active links
+    $('#nbl-' + currentPage).addClass('active');
+
+});
