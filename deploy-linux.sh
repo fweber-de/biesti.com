c4e33daabd1b4d668e90b228664031b29f52635b
Florian Weber <florian.weber.dd@icloud.com>
2015-04-12 09:48:27 +0200

    deploy

--- deploy-linux.sh
+++ deploy-linux.sh
@@ -2,6 +2,8 @@ git stash
 git pull
 php app/console doctrine:schema:update --force
 php app/console cache:clear --env=prod --no-debug
+rm -rf web/js
+rm -rf web/css
 bower prune --allow-root
 bower install --allow-root
 php app/console assetic:dump --env=prod --no-debug
