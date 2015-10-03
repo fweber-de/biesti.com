d7405779dbf9b9b80376660cca79207942879cd6
Florian Weber <florian.weber.dd@icloud.com>
2015-10-03 14:13:49 +0200

    deploy.bat fix

--- deploy-windows.bat
+++ deploy-windows.bat
@@ -3,4 +3,5 @@ php app\console doctrine:schema:update --force
 php app\console cache:clear --env=prod --no-debug
 bower prune
 bower install
+php app/console assetic:dump --env=prod --no-debug
 composer install
